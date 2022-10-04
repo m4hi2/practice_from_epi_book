7bfb05e58b9c97f0fe229f870876867416949e30
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2022-10-04 15:52:04 +0600

    no need to get the rightmost bit

--- right_propagate_rightmost_set_bit.py
+++ right_propagate_rightmost_set_bit.py
@@ -1,9 +1,7 @@
 # Right propagate the rightmost set bit in x, e.9., tums (01010000)2 to (01011111)2
 
 def right_prop(number):
-    right_most_set_bit = number & ~(number-1)
-
-    return number | right_most_set_bit - 1
+    return number | number - 1
 
 
 for i in range(80, 100):
